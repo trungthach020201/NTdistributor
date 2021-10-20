b649829fe5c3b1a930b1e65a10e6b85991057c40
trungthach020201 <wendythach489@gmail.com>
2021-10-20 20:25:24 +0700

    connect1

--- connection.php
+++ connection.php
@@ -1,4 +1,6 @@
 <?php
-    $conn = mysqli_connect('localhost','root','','NTdistributor')
-    or die("can not connect data base".mysqli_connect_error());
+    $Connect = pg_connect("postgres://zarjhjfdlyasvx:faf4d6773d9132d2c6836a174c322162a3220a61c404ca187c3e770479cfd47f@ec2-3-218-47-9.compute-1.amazonaws.com:5432/dab0kdsrv2qish")
+     or die("can not connect data base".pg_connect_error());
+
+    }
 ?>
