2987a82a1351c5d3bc541bf4221affe94f650e69
Thach Ngoc Trung <91512874+trungthach020201@users.noreply.github.com>
2021-10-23 09:45:20 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,4 @@
 <?php
-     $conn = pg_connect("postgres://cnvnkjrbeouslw:7d7643cd9914d45b8ced65763810353398bbab37d465e252ddae3847d1426988@ec2-44-199-19-76.compute-1.amazonaws.com:5432/d282ipi4c4ru18
-") 
+     $conn = pg_connect("postgres://cnvnkjrbeouslw:7d7643cd9914d45b8ced65763810353398bbab37d465e252ddae3847d1426988@ec2-44-199-19-76.compute-1.amazonaws.com:5432/d282ipi4c4ru18"); 
     if (!$conn){die echo "failed";}
  ?>
