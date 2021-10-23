8c31c91d6eec82cb347824bed5ce4f497b999dcc
trungthach020201 <wendythach489@gmail.com>
2021-10-23 09:36:38 +0700

    home124

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-     $Connect = pg_connect("postgres://cnvnkjrbeouslw:7d7643cd9914d45b8ced65763810353398bbab37d465e252ddae3847d1426988@ec2-44-199-19-76.compute-1.amazonaws.com:5432/d282ipi4c4ru18
-     ") or die("Lỗi".pg_error($Connect));
-     pg_query($Connect,'SET NAMES "utf8"');
+     $conn = pg_connect("postgres://cnvnkjrbeouslw:7d7643cd9914d45b8ced65763810353398bbab37d465e252ddae3847d1426988@ec2-44-199-19-76.compute-1.amazonaws.com:5432/d282ipi4c4ru18
+     ") or die("Lỗi".pg_error($conn));
+     pg_query($conn,'SET NAMES "utf8"');
  ?>
